bba0abadc76ad3887362f0a3b2401513c3e8e878
GHANNATH KUMAR <ghannathk@gmail.com>
2025-02-16 15:27:52 +0530

    ADD

---
+++ main1.js
@@ -0,0 +1,11 @@
+import {a} from "./mymodule.js"
+console.log(a);
+
+import obj from "./mymodule.js" // obj ke jagah  kuch bhi rah sakta tha name because by default // note that curly bracket not here
+console.log(obj);
+// const t=require("./mymodule.js") // without type module / 
+// console.log(t); // t =56
+
+// (function(exports ,require,module,_filename,_dirname){
+//     //module codes actually              
+// })
---
+++ mymodule.js
@@ -0,0 +1,10 @@
+export const a = 1; //named export 
+
+const obj ={
+    x:5,
+    y:7
+}
+export default obj;
+
+// let p=56;
+// module.exports=p; 
